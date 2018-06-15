6663e8d656f83b69bf14ca41252f9a38fb649ffe
Eugene Ivantsov <eivantsov@redhat.com>
2018-06-15 14:18:41 +0300

    Java 10 example

---
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.eclipse.che.examples;
+
+public class HelloWorld {
+    public static void main(String... argvs) {
+        var a = new String("Che");
+        System.out.println("Hello World " + a + "!");
+    }
+}
